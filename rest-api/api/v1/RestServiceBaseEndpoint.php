64d4e480f6325a935677186c78c4b3ba8dfccb37
Moorkens <Bas.Moorkens@ordina.be>
2019-04-13 10:42:09 +0200

    added header authentication for v cc o

--- rest-api/api/v1/RestServiceBaseEndpoint.php
+++ rest-api/api/v1/RestServiceBaseEndpoint.php
@@ -6,8 +6,13 @@
 class RestServiceBaseEndpoint
 {
 
+    var $v;
+    var $cc;
+    var $o;
+    
     function handleRequest($requestMethod)
     {
+        $this->handleHeaderAuthentication();
         switch ($requestMethod) {
             case 'GET':
                 $this->handleGetRequest();
@@ -23,6 +28,38 @@ class RestServiceBaseEndpoint
         }
     }
 
+    /**
+     * In order to authenticate to the system we need to have following headers present on each request:
+     * v: vendor domain name
+     * cc: vendor country id
+     * o: user id hash
+     * 
+     * If any of these headers is not present we will redirect with a 401 unautherized response. 
+     */
+    function handleHeaderAuthentication() {
+        foreach (getallheaders() as $name => $value) {
+            if($name == 'v') {
+                $this->v = $value;
+            } else if($name == 'cc') {
+                $this->cc = $value;
+            } else if($name == 'o') {
+                $this->o = $value;
+            }
+        } 
+        if( empty($this->v)) {
+            header('HTTP/1.0 401 Header paremeter v is required for authentication');
+            die();
+        }
+        if( empty($this->cc)) {
+            header('HTTP/1.0 401 Header paremeter cc is required for authentication');
+            die();
+        }
+        if( empty($this->o)) {
+            header('HTTP/1.0 401 Header paremeter o is required for authentication');
+            die();
+        }
+    }
+    
     function handleGetRequest()
     {
         header("HTTP/1.0 501 Not found");
